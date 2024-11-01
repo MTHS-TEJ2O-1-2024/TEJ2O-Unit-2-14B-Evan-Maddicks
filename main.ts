3bdb9a09e35ecb21a8f114521fd7c2ea2bd7fb60
Evan-Maddicks <evan.maddicks@mths.ca>
2024-11-01 10:44:44 -0400

    draws a perimeter clockwise and counter-clockwise

--- main.ts
+++ main.ts
@@ -1,8 +1,74 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan M
+ * Created on: Oct 2024
+ * This program will draw a perimeter both clockwise and counter-clockwise
 */
 
-basic.showString('Hello, World!')
+//variables
+let loopNumberX = 0
+let loopNumberY = 0
+let sprite: game.LedSprite = null
+
+//setup
+basic.clearScreen()
+basic.pause(500)
+basic.showIcon(IconNames.Happy)
+
+//loop a
+input.onButtonPressed(Button.A, function() {
+    // setup
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+
+    while (loopNumberX <= 5) {
+        sprite.set(LedSpriteProperty.X, loopNumberX)
+        loopNumberX = loopNumberX + 1
+        basic.pause(250)
+    }
+    while (loopNumberY <= 5) {
+        sprite.set(LedSpriteProperty.Y, loopNumberY)
+        loopNumberY = loopNumberY + 1
+        basic.pause(250)
+    }
+    while (loopNumberX >= 0) {
+        sprite.set(LedSpriteProperty.X, loopNumberX)
+        loopNumberX = loopNumberX - 1
+        basic.pause(250)
+    }
+    while (loopNumberY >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopNumberY)
+        loopNumberY = loopNumberY - 1
+        basic.pause(250)
+    }
+})
+
+//loop b
+input.onButtonPressed(Button.B, function () {
+    // setup
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+
+    while (loopNumberX <= 5) {
+        sprite.set(LedSpriteProperty.X, loopNumberX)
+        loopNumberX = loopNumberX + 1
+        basic.pause(250)
+
+        while (loopNumberY <= 5) {
+            sprite.set(LedSpriteProperty.Y, loopNumberY)
+            loopNumberY = loopNumberY + 1
+            basic.pause(250)
+        }
+    }
+    while (loopNumberX >= 0) {
+        sprite.set(LedSpriteProperty.X, loopNumberX)
+        loopNumberX = loopNumberX - 1
+        basic.pause(250)
+
+        while (loopNumberY >= 0) {
+            sprite.set(LedSpriteProperty.Y, loopNumberY)
+            loopNumberY = loopNumberY - 1
+            basic.pause(250)
+        }
+    }
+})
